1041ea412485f4fd16f87e433b774eecfc727238
Sean King <sean@seanking.co>
2020-11-21 19:34:37 +1300

    Add prompt types

--- saofile.js
+++ saofile.js
@@ -3,26 +3,32 @@ module.exports = {
     return [
       {
         name: 'name',
+        type: 'input',
         message: `Component name:`
       },
       {
         name: 'packageName',
+        type: 'input',
         message: `Package name:`
       },
       {
         name: 'title',
+        type: 'input',
         message: 'Human friendly title:'
       },
       {
         name: 'description',
+        type: 'input',
         message: 'Describe it:'
       },
       {
         name: 'author',
+        type: 'input',
         message: 'Author:'
       },
       {
         name: 'githubOrg',
+        type: 'input',
         message: 'GitHub org:'
       }
     ];
