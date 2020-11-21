b16c00f2b2dd06ffea36a7b712c39f37361628bc
Sean King <sean@seanking.co>
2020-11-21 19:44:27 +1300

    Revert to sao 1.x

--- saofile.js
+++ saofile.js
@@ -1,32 +1,26 @@
 module.exports = {
   prompts: [
     {
-      type: 'input',
       name: 'name',
       message: `Component name:`
     },
     {
-      type: 'input',
       name: 'packageName',
       message: `Package name:`
     },
     {
-      type: 'input',
       name: 'title',
       message: 'Human friendly title:'
     },
     {
-      type: 'input',
       name: 'description',
       message: 'Describe it:'
     },
     {
-      type: 'input',
       name: 'author',
       message: 'Author:'
     },
     {
-      type: 'input',
       name: 'githubOrg',
       message: 'GitHub org:'
     }
