1982b73b11e38ebb42f2139442758750b9889f52
Sean King <sean@seanking.co>
2020-11-21 19:41:40 +1300

    No need for prompts to be a function

--- saofile.js
+++ saofile.js
@@ -1,38 +1,36 @@
 module.exports = {
-  prompts() {
-    return [
-      {
-        name: 'name',
-        type: 'input',
-        message: `Component name:`
-      },
-      {
-        name: 'packageName',
-        type: 'input',
-        message: `Package name:`
-      },
-      {
-        name: 'title',
-        type: 'input',
-        message: 'Human friendly title:'
-      },
-      {
-        name: 'description',
-        type: 'input',
-        message: 'Describe it:'
-      },
-      {
-        name: 'author',
-        type: 'input',
-        message: 'Author:'
-      },
-      {
-        name: 'githubOrg',
-        type: 'input',
-        message: 'GitHub org:'
-      }
-    ];
-  },
+  prompts: [
+    {
+      name: 'name',
+      type: 'input',
+      message: `Component name:`
+    },
+    {
+      name: 'packageName',
+      type: 'input',
+      message: `Package name:`
+    },
+    {
+      name: 'title',
+      type: 'input',
+      message: 'Human friendly title:'
+    },
+    {
+      name: 'description',
+      type: 'input',
+      message: 'Describe it:'
+    },
+    {
+      name: 'author',
+      type: 'input',
+      message: 'Author:'
+    },
+    {
+      name: 'githubOrg',
+      type: 'input',
+      message: 'GitHub org:'
+    }
+  ],
   actions: [
     {
       type: 'add',
